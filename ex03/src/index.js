8db84b9805e234b65ba6de89170b042771edd2f6
sara-gheith <saraannegheith@gmail.com>
2021-03-25 03:06:18 +0100

    Destructuring Assignment to Assign Variables

---
+++ ex03/src/index.js
@@ -0,0 +1,16 @@
+const MY_CAR = {
+    wheels: 4,
+    cylinders: 4,
+    size: 2.2
+};
+
+function main(myCar) {
+    
+    const { cylinders: carCylinders, size: carSize } = MY_CAR;
+
+    return {carCylinders, carSize}
+
+}
+
+console.log(main(MY_CAR));
+module.exports = main;
